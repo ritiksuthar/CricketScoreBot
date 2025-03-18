34ecffd6bb60d65ca63257e71c7c018acfbb9989
theritiksuthar <63701883+ritiksuthar@users.noreply.github.com>
2025-03-18 14:56:46 +0530

    Update bot.py

--- telegram_bot/src/bot.py
+++ telegram_bot/src/bot.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
 from config import BOT_API_TOKEN
 from handlers import start, help_command, score, subscribe, unsubscribe
+import os
 
 def main():
     app = ApplicationBuilder().token(BOT_API_TOKEN).build()
@@ -15,7 +16,9 @@ def main():
 
 
     print("Bot is running...")
-    app.run_polling()
+    # Port Configuration for Render Deployment
+    port = int(os.environ.get("PORT", 5000))
+    app.run_polling(port=port)
 
 if __name__ == "__main__":
     main()
